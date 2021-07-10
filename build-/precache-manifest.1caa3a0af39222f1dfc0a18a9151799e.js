750386a9f5bc45529a7401543c2245d54b2ad372
Fady <oursrapieure89@gmail.com>
2021-07-10 15:51:14 +0100

    fixed a bunch of stuff

---
+++ build-/precache-manifest.1caa3a0af39222f1dfc0a18a9151799e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb4506e143e6150827639cae731f443",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd8f93ba14bc95f42ddf",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "cd8f93ba14bc95f42ddf",
+    "url": "/static/js/2.3f82ca8e.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/static/js/2.3f82ca8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbfab5c77ceb99492579",
+    "url": "/static/js/main.6d5fb5aa.chunk.js"
+  },
+  {
+    "revision": "aeea36bdadbfd99c7e94",
+    "url": "/static/js/runtime-main.f7969a42.js"
+  },
+  {
+    "revision": "69cc9f8dae6f4315f568172912bfaedf",
+    "url": "/static/media/logo.69cc9f8d.png"
+  },
+  {
+    "revision": "6dbba458959d4ce1edd2f5b3ab3ae13b",
+    "url": "/static/media/logo.6dbba458.svg"
+  }
+]);
